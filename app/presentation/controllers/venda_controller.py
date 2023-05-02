a509e165108db7a520310c1364975c06fe88b635
icaro moroni <icaromorone@gmail.com>
2023-05-01 23:04:17 -0300

    função obter vendar ok

--- app/presentation/controllers/venda_controller.py
+++ app/presentation/controllers/venda_controller.py
@@ -1,17 +1,18 @@
 from fastapi import APIRouter, Depends, HTTPException, status
-from app.persistence.repository_venda_mongodb import RepositoryVendaMongoDB
+from app.persistence.repository_venda_mongodb import VendaRepositoryMongoDB
 from ..viewmodels import Venda
 
 print('Venda controller ok!')
 routes = APIRouter()
 prefix = '/vendas'
 
-repository_venda = RepositoryVendaMongoDB()
+venda_repository = VendaRepositoryMongoDB()
 
 @routes.get('/')
-def obter_vendas():
-    pass
+def obter_vendas(skip: int | None = 0, take: int | None = 0):
+    vendas = venda_repository.todas(skip,take)
+    return vendas
 
 @routes.post('/', status_code=status.HTTP_201_CREATED)
 def nova_venda(venda: Venda):
-    return repository_venda.salvar(venda)
+    return venda_repository.salvar(venda)
